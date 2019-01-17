5bfc31902b770bd66ee6fdc5c509b69bcfcacdfa
Yuuya Toyoda <yuuya.s.toyoda@jalinfotec.co.jp>
2019-01-17 19:14:24 +0900

    BootAppの配置階層のミスを修正

--- taxiReservation/src/main/kotlin/com/jalinfotec/soraguide/taxi/taxiReservation/BootApp.kt
+++ taxiReservation/src/main/kotlin/com/jalinfotec/soraguide/taxi/taxiReservation/BootApp.kt
@@ -1,4 +1,4 @@
-package com.jalinfotec.soraguide.taxi.taxiReservation.controller
+package com.jalinfotec.soraguide.taxi.taxiReservation
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer

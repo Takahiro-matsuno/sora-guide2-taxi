1c16b8e9594dd25cfe125b987ba40d84ff467c63
Sho Hashimoto <shou.s.hashimoto@jalinfotec.co.jp>
2019-01-30 15:15:56 +0900

    [add]タクシー会社サイトのプロジェクトを追加

---
+++ taxiCompanyApp/src/main/kotlin/com/jalinfotec/soraguide/taxi/company/taxiCompanyApp/TaxiCompanyApplication.kt
@@ -0,0 +1,12 @@
+package com.jalinfotec.soraguide.taxi.company.taxiCompanyApp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaxiCompanyAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaxiCompanyAppApplication>(*args)
+}
+
---
+++ taxiCompanyApp/src/main/kotlin/com/jalinfotec/soraguide/taxi/company/taxiCompanyApp/controller/AuthController.kt
@@ -0,0 +1,8 @@
+package com.jalinfotec.soraguide.taxi.company.taxiCompanyApp.controller
+
+import org.springframework.stereotype.Controller
+
+@Controller
+class AuthController {
+
+}

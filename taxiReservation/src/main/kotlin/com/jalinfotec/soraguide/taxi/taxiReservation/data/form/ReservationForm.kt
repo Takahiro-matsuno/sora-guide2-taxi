17ef03a6393a8e40fb3d73268c8c298afd2c523e
hidetoKOJIMA <hideto.s.kojima@jalinfotec.co.jp>
2019-02-01 15:58:13 +0900

    予約確認画面をコントローラと連携できるようにした

--- taxiReservation/src/main/kotlin/com/jalinfotec/soraguide/taxi/taxiReservation/data/form/ReservationForm.kt
+++ taxiReservation/src/main/kotlin/com/jalinfotec/soraguide/taxi/taxiReservation/data/form/ReservationForm.kt
@@ -2,19 +2,51 @@ package com.jalinfotec.soraguide.taxi.taxiReservation.data.form
 
 import java.sql.Time
 import java.sql.Date
+import java.util.*
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 data class ReservationForm(
-    var date: Date = Date(0),
+    //初期値:本日の日付
+        @NotNull
+    var date: Date = Date(Calendar.getInstance().getTimeInMillis()),
+    //??
+        @NotEmpty
     var time: String = Time(date.time).toString(),
-    var adult: Int = 0,
+
+        @NotEmpty
+    var adult: Int = 1,
+
+        @NotEmpty
     var child: Int = 0,
-    var taxi_number: Int = 0,
+
+        @NotEmpty
+    var taxi_number: Int = 1,
+
+        @NotNull
     var company_id: String = "",
+
+        @NotNull
     var destination: String = "",
+
+        @NotNull
     var name: String = "",
+
+        @NotNull
     var phonetic: String = "",
+
+        @NotNull
     var phone: String = "",
+
+        @NotEmpty
+    @Email
     var mail: String = "",
+
+        @NotEmpty
+    @Email
     var mailCheck: String = "",
-    var comment: String = ""
+
+        //任意項目
+        var comment: String = ""
 )

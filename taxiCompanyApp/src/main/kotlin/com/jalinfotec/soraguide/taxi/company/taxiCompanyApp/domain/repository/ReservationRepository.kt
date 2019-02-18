61255d55dfbb11f1ec29713a2e244a0beeb0d5cb
Sho Hashimoto <shou.s.hashimoto@jalinfotec.co.jp>
2019-02-18 16:03:00 +0900

    [update]ReservationRepositoryの変数名を修正

--- taxiCompanyApp/src/main/kotlin/com/jalinfotec/soraguide/taxi/company/taxiCompanyApp/domain/repository/ReservationRepository.kt
+++ taxiCompanyApp/src/main/kotlin/com/jalinfotec/soraguide/taxi/company/taxiCompanyApp/domain/repository/ReservationRepository.kt
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository
 @Repository
 interface ReservationRepository : JpaRepository<ReservationInformation, String> {
     // 参考：https://qiita.com/sndr/items/af7d12be264c2cc4b252
-    fun findByCompanyIdOrderByDateAscTimeAsc(company_id: String):MutableList<ReservationInformation>
-    fun findByCompanyIdAndId(company_id: String, reservationId: String): ReservationInformation?
+    fun findByCompanyIdOrderByDateAscTimeAsc(companyId: String):MutableList<ReservationInformation>
+    fun findByCompanyIdAndId(companyId: String, reservationId: String): ReservationInformation?
 }

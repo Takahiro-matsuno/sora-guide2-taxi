04ffe62f3073f16b6c57b3d86dfdd40f5f257cbc
Sho Hashimoto <shou.s.hashimoto@jalinfotec.co.jp>
2019-02-18 16:23:30 +0900

    [update]ReservationFormのフィールド名を修正

--- taxiCompanyApp/src/main/kotlin/com/jalinfotec/soraguide/taxi/company/taxiCompanyApp/domain/form/ReservationForm.kt
+++ taxiCompanyApp/src/main/kotlin/com/jalinfotec/soraguide/taxi/company/taxiCompanyApp/domain/form/ReservationForm.kt
@@ -4,21 +4,21 @@ import java.sql.Date
 import java.sql.Time
 
 class ReservationForm(
-        var id: String = "",
-        var status: Int = 0,
-        var date: Date? = null,
-        var time: Time? = null,
+        var reservationId: String = "",
+        var statusName: String = "",
+        var rideOnDate: Date,
+        var rideOnTime: Time,
         var adult: Int = 0,
         var child: Int = 0,
-        var taxi_number: Int = 0,
+        var taxiNumber: Int = 0,
         var companyId: String = "",
         var destination: String = "",
-        var name: String = "",
-        var phonetic: String = "",
-        var phone: String = "",
-        var mail: String = "",
+        var passengerName: String = "",
+        var passengerPhonetic: String = "",
+        var passengerContact: String = "",
+        var passengerMail: String = "",
         var comment: String = "",
-        var car_number: String = "",
-        var car_contact: String = "",
+        var carNumber: String = "",
+        var carContact: String = "",
         var notice: String = ""
 )

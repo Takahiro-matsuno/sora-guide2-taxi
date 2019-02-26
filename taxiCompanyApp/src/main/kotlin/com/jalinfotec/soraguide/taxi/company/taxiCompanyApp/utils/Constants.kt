bf20d27bbc4c7497b07033b512e06cc5b2e3cb8a
Sho Hashimoto <shou.s.hashimoto@jalinfotec.co.jp>
2019-02-26 18:18:42 +0900

    [add]メール送信機能を追加

--- taxiCompanyApp/src/main/kotlin/com/jalinfotec/soraguide/taxi/company/taxiCompanyApp/utils/Constants.kt
+++ taxiCompanyApp/src/main/kotlin/com/jalinfotec/soraguide/taxi/company/taxiCompanyApp/utils/Constants.kt
@@ -1,11 +1,17 @@
 package com.jalinfotec.soraguide.taxi.company.taxiCompanyApp.utils
 
 object Constants {
+    // TODO 入力値の制限は？
     val PAX_RANGE = 0..20
+
+    // TODO ステータスの更新のユースケース
     val reservationStatus = mutableMapOf(
             1 to "受付中",
             2 to "予約確定",
             3 to "配車中",
             4 to "キャンセル済み",
             5 to "確定")
+
+    // 環境変数から取得する
+    val FROM_ADDRESS = System.getenv("FROM_ADDRESS")
 }
